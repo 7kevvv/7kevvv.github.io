8ba49e487ca08d75cd8c341a9d294297c77cc986
7kevvv <114033637+7kevvv@users.noreply.github.com>
2022-10-09 18:24:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-//Testing Javascript.
+// Testing Javascript.
+start();
+endAlert();
 
-alert("Yoo, welcome to my website please give me suggestions in DMS on what to add!");
+
+function start() { 
+alert("Hi, welcome to my website, please note that it looks the best on zoom 100%, (default), and on computer.");
+console.log('Printed Alert');
+}
+
+function endAlert() {
+    alert("note that this is in development as well there may be bugs or incomplete features, press OK to continue to the website.")
+    console.log('Welcome to my website, enjoy!')
+
+}
