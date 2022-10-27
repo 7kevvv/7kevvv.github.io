ef33b1fe219268a959010d4fa26319c0d784f021
Chaqken <114033637+Chaqken@users.noreply.github.com>
2022-10-26 20:22:28 -0400

    Update and rename code.js to script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+
+
+(function($){
+  
+    var circleSingletonClass =(function(){
+      var instance;
+      
+      function init(){
+        var _aCircle =[],
+            _stage = $('body');
+            
+            function _position(circle,top,left){
+               circle.css('top',top);
+               circle.css('left',left);
+            }
+            
+            function create(top,left){
+               var circle=$('<div class="circle"></div>');
+               _position(circle,top,left)
+               return circle;
+            }
+            
+            function add(circle){
+              _stage.append(circle);
+              _aCircle.push(circle);
+            }
+        return {
+          create:create,
+          add:add
+          
+        }
+      }
+      
+      return {
+        getInstance:function(){
+          if(!instance){
+            instance = init()
+          }
+          return instance;
+        }
+        
+      }
+      
+    })();
+    
+    
+    
+    
+    $(function(){
+    $('#fx').click(function(e){
+      var cg =circleSingletonClass.getInstance();
+      var circle =cg.create(e.pageY-10,e.pageX-10)
+      cg.add(circle)
+    })
+  })
+  })(jQuery)
+
+  function refereshPage(){
+    window.location.reload();
+  }
+
+  function submitArt(){
+    window.open('https://discord.gg/wgvGFmgCyd')
+  }
